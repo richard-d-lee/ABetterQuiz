094c8e2443ca31cf4e3837ea76b3eb4ad7aeb922
richard-d-lee <richdelee@gmail.com>
2020-12-08 18:40:19 -0700

    Added a question

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import quiz from '/components/quiz.js';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
     return (
         <div>
             Hello World!
+            <Card>
+                hello
+            </Card>
+            <Button>
+                hello
+            </Button>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
+
     );
 }
 

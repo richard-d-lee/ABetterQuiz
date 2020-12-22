226fd74ab8955ec701ddfc0d6f261c18f9362ac0
richard-d-lee <richdelee@gmail.com>
2020-12-22 09:48:06 -0700

    Front end adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,24 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            countryList: [],
+            currentCountry: '',
+            linkRef: '',
+            currentImage: ''
         }
+    }
+    componentDidMount() {
+        let ntryList = Object.keys(quiz);
+        this.setState({
+            countryList: ntryList,
+            currentCountry: ntryList[2],
+        }, () => {
+            console.log(this.state.currentCountry);
+            this.setState({
+                linkRef: `https://www.google.com/search?q=${this.state.currentCountry}`,
+                currentImage: quiz[this.state.currentCountry].image,
+            })
+        })
     }
     render() {
 
@@ -18,15 +34,21 @@ class App extends React.Component {
             <div>
                 <div className="topBar">
                     <div className="banner">
-                        <Placeholder name={'banner'} />
+                        <Placeholder name={'Quistory'} />
+                    </div>
+                    <div className="header">
+                        <Placeholder name={'Scroll down to visit a random country!'}/>
                     </div>
                     <div className="login">
                         <Placeholder name={'login'} />
                     </div>
                 </div>
                 <div className="get-started-button">
-                    <h1>Get Started!</h1>
+                    <h1 className="start-text">Get Started!</h1>
                 </div>
+                <a href={this.state.linkRef}>
+                    <img className="image" src={this.state.currentImage}></img>
+                </a>
             </div>
 
         );
